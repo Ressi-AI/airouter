ca02320ba79d3182cf88185b6b514db1d98594cc
Laurentiu Piciu <laurentiupiciu@gmail.com>
2024-03-24 16:02:46 +0200

    exclude providers installs from requirements; each project that uses airouter should install the required providers

--- airouter/providers/factory.py
+++ airouter/providers/factory.py
@@ -1,11 +1,6 @@
 import typing as t
 from airouter.models import ProviderName
 from airouter.providers.base_provider import BaseProvider
-from airouter.providers.openai_provider import OpenAIProvider
-from airouter.providers.aws_bedrock_provider import AWSBedrockProvider
-from airouter.providers.vertex_ai_text_provider import VertexAITextProvider
-from airouter.providers.ollama_provider import OllamaProvider
-from airouter.providers.azure_openai_provider import AzureOpenAIProvider
 
 
 def provider_factory(provider_name, **llm_params) -> t.Optional[BaseProvider]:
@@ -13,17 +8,23 @@ def provider_factory(provider_name, **llm_params) -> t.Optional[BaseProvider]:
     provider_name = ProviderName.from_str(provider_name)
 
   if provider_name == ProviderName.OPENAI:
+    from airouter.providers.openai_provider import OpenAIProvider
     return OpenAIProvider(stream=True, **llm_params)
 
   if provider_name == ProviderName.AWS_BEDROCK:
+    from airouter.providers.aws_bedrock_provider import AWSBedrockProvider
     return AWSBedrockProvider(stream=True, **llm_params)
 
   if provider_name == ProviderName.VERTEX_AI_TEXT:
+    from airouter.providers.vertex_ai_text_provider import VertexAITextProvider
     return VertexAITextProvider(stream=True, **llm_params)
 
   if provider_name == ProviderName.OLLAMA:
+    from airouter.providers.ollama_provider import OllamaProvider
     return OllamaProvider(stream=True, **llm_params)
+
   if provider_name == ProviderName.AZURE_OPENAI:
+    from airouter.providers.azure_openai_provider import AzureOpenAIProvider
     return AzureOpenAIProvider(stream=True, **llm_params)
 
   return
